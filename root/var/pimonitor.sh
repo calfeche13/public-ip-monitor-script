04085c49cf6921a1a9f5c3f1886d0caaa2702e1d
chosenrealmalfeche <chosen.alfeche@gmail.com>
2024-09-03 22:01:10 +0800

    Fixed UCI option not found, moved monitor checking before configuring procd, added monitoring for both ipv4 and ipv6 to distinguish from each other.

--- root/var/pimonitor.sh
+++ root/var/pimonitor.sh
@@ -1,17 +1,17 @@
-MONITOR=$1
+SCRIPT_FOR=$1
 CURRENT_IP_UCI_OPTION=$2
 SERVICE=$3
 INTERVAL=$4
 HANDLER_SCRIPT_PATH=$5
 HISTORY_PATH=$6
 
-echo "Starting PIMONITOR script"
-while [ $MONITOR -eq 1 ]; do
+echo "Starting PIMONITOR for ${SCRIPT_FOR} script"
+while [ 1 ]; do
     RETRIEVED_IP=$(curl -s "${SERVICE}")
-    CURRENT_IP=$(uci get $CURRENT_IP_UCI_OPTION)
+    CURRENT_IP=$(uci -q get "${CURRENT_IP_UCI_OPTION}")
     if [ "$CURRENT_IP" != "${RETRIEVED_IP}" ]; then
         echo "IP changed from ${CURRENT_IP} to ${RETRIEVED_IP}"
-        $HANDLER_SCRIPT_PATH $RETRIEVED_IP
+        sh $HANDLER_SCRIPT_PATH $RETRIEVED_IP
         uci set ${CURRENT_IP_UCI_OPTION}=$RETRIEVED_IP
         uci commit
     fi
